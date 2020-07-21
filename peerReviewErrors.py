73fd568e1685741f7f02722fea134030bf027f3f
Jlong <jacoblongfield@gmail.com>
2020-07-21 17:22:25 -0500

    Update peerReviewErrors.py

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,8 +1,8 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: <Jacob Longfield>
+# Creation Date: <7/21/2020>
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
@@ -16,12 +16,11 @@ def displayIntro():
 	print()
 
 def chooseCave():
-    cave = ''
-	while cave != '1' and cave != '2':
+	cave = ''
+	while (cave != '1') and (cave != '2'): 
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
-
-	return caves
+	return cave #Was caves needs to be cave to match variable name
 
 def checkCave(chosenCave):
 	print('You approach the cave...')
@@ -29,7 +28,7 @@ def checkCave(chosenCave):
 	time.sleep(2)
 	print('It is dark and spooky...')
 	#sleep for 2 seconds
-	time.sleep(3)
+	time.sleep(2) #Says to sleep for 2 seconds but parameter is 3
 	print('A large dragon jumps out in front of you! He opens his jaws and...')
 	print()
 	#sleep for 2 seconds
@@ -37,18 +36,19 @@ def checkCave(chosenCave):
 	friendlyCave = random.randint(1, 2)
 
 	if chosenCave == str(friendlyCave):
-		print('Gives you his treasure!')
+		print('Gives you his treasure!') # I do get both messages when the game is played
 	else:
-		print 'Gobbles you down in one bite!'
+		print ('Gobbles you down in one bite!') #Missing ()
 
 playAgain = 'yes'
-while playAgain = 'yes' or playAgain = 'y':
+while playAgain == 'yes' or playAgain == 'y': # Use == to check if something is equal x2
 	displayIntro()
-	caveNumber = choosecave()
+	caveNumber = chooseCave() #Need to match method name. Was choosecave()
 	checkCave(caveNumber)
     
 	print('Do you want to play again? (yes or no)')
-	playAgain = input()
-	if playAgain == "no":
-		print("Thanks for planing")
+	playAgain = input().lower()
+	if playAgain == "no" or playAgain == 'n': #Message only appears if user types in no, for yes it also checks for y
+		print("Thanks for playing") #Playing is spelled wrong
 
+# Program quits if 'Yes' is used or if 'Y' is used. Use .lower() to take care of that
